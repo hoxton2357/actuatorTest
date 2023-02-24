ed71b8cbf6f96debf4d5ad615eb91bc1d6d5f8cc
Hoxton Hsu <hoxton.hsu@eunodata.com>
2023-02-24 09:52:11 +0800

    done

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -11,16 +11,42 @@ stompClient.connect({}, function (frame) {
         // 取得各組件的狀態
         var cpuStatus = data.components.CPUStatus.status;
         var processCpuLoad = data.components.CPUStatus.details.processCpuLoad;
+        var arch = data.components.CPUStatus.details.arch;
+        var name = data.components.CPUStatus.details.name
+        var condition = data.components.CPUStatus.details.condition
 
         var diskSpace = data.components.diskSpace.status;
+        var total = data.components.diskSpace.details.total;
+        var free = data.components.diskSpace.details.free;
+        var threshold = data.components.diskSpace.details.threshold;
+
         var memory = data.components.memory.status;
+        var totalPhysicalMemorySize = data.components.memory.details.totalPhysicalMemorySize;
+        var usedPhysicalMemorySize = data.components.memory.details.usedPhysicalMemorySize;
+        var freePhysicalMemorySize = data.components.memory.details.freePhysicalMemorySize;
+        var condition2 = data.components.memory.details.condition;
+
+
         var ping = data.components.ping.status;
 
         // 將狀態資訊顯示在頁面上
         document.getElementById('cpu-status').innerHTML = 'CPU狀態：' + cpuStatus;
-        document.getElementById('cpu-status-processCPU').innerHTML = 'CPU負荷比率' + processCpuLoad;
+        document.getElementById('cpu-status-processCPU').innerHTML = 'CPU負荷比率：' + processCpuLoad;
+        document.getElementById('cpu-arch').innerHTML = 'CPU架構：' + arch;
+        document.getElementById('cpu-name').innerHTML = '作業系統名稱：' + name;
+        document.getElementById('cpu-condition').innerHTML = '描述：' + condition;
+
         document.getElementById('disk-space').innerHTML = '磁碟空間狀態：' + diskSpace;
+        document.getElementById('disk-total').innerHTML = '磁碟總空間：' + total;
+        document.getElementById('disk-free').innerHTML = '磁碟剩餘狀態：' + free;
+        document.getElementById('disk-threshold').innerHTML = '磁碟閾值：' + threshold;
+
         document.getElementById('memory').innerHTML = '記憶體狀態：' + memory;
+        document.getElementById('memory-totalPhysicalMemorySize').innerHTML = '記憶體總空間：' + totalPhysicalMemorySize;
+        document.getElementById('memory-usedPhysicalMemorySize').innerHTML = '記憶體已使用：' + freePhysicalMemorySize;
+        document.getElementById('memory-freePhysicalMemorySize').innerHTML = '自由記憶體：' + usedPhysicalMemorySize;
+        document.getElementById('memory-condition').innerHTML = '描述：' + condition2;
+
         document.getElementById('ping').innerHTML = 'Ping狀態：' + ping;
     });
     stompClient.send('/app/health', {}, '');
